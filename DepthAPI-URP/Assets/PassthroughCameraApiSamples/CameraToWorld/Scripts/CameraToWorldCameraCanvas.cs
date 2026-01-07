66fc232d9b61f2fd2d3ceaaf48be1a2ce16d1f78
Riccardo Bovo <rb1619@ic.ac.uk>
2026-01-07 13:09:49 +0000

    update CameraToWorldCameraCanvas to use WebCamTextureManager

--- DepthAPI-URP/Assets/PassthroughCameraApiSamples/CameraToWorld/Scripts/CameraToWorldCameraCanvas.cs
+++ DepthAPI-URP/Assets/PassthroughCameraApiSamples/CameraToWorld/Scripts/CameraToWorldCameraCanvas.cs
@@ -1,8 +1,8 @@
 // Copyright (c) Meta Platforms, Inc. and affiliates.
 
 using System.Collections;
-using Meta.XR;
 using Meta.XR.Samples;
+using PassthroughCameraSamples;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,61 +11,178 @@ namespace PassthroughCameraSamples.CameraToWorld
     [MetaCodeSample("PassthroughCameraApiSamples-CameraToWorld")]
     public class CameraToWorldCameraCanvas : MonoBehaviour
     {
-        [SerializeField] private PassthroughCameraAccess m_cameraAccess;
+        [SerializeField] private WebCamTextureManager m_webCamTextureManager;
         [SerializeField] private Text m_debugText;
         [SerializeField] private RawImage m_image;
         private Texture2D m_cameraSnapshot;
+        private Coroutine m_streamCoroutine;
+        private bool m_isCapturing;
 
         public void MakeCameraSnapshot()
         {
-            if (!m_cameraAccess.IsPlaying)
+            if (m_isCapturing)
             {
-                Debug.LogError("!m_cameraAccess.IsPlaying");
                 return;
             }
-
-            if (m_cameraSnapshot == null)
-            {
-                var size = m_cameraAccess.CurrentResolution;
-                m_cameraSnapshot = new Texture2D(size.x, size.y, TextureFormat.RGBA32, false);
-            }
-
-            var pixels = m_cameraAccess.GetColors();
-            m_cameraSnapshot.LoadRawTextureData(pixels);
-            m_cameraSnapshot.Apply();
-
-            StopCoroutine(ResumeStreamingFromCameraCor());
-            m_image.texture = m_cameraSnapshot;
+            StartCoroutine(CaptureSnapshotCoroutine());
         }
 
         public void ResumeStreamingFromCamera()
         {
-            StartCoroutine(ResumeStreamingFromCameraCor());
+            if (m_streamCoroutine != null)
+            {
+                StopCoroutine(m_streamCoroutine);
+            }
+            m_streamCoroutine = StartCoroutine(ResumeStreamingFromCameraCor());
         }
 
         private IEnumerator ResumeStreamingFromCameraCor()
         {
-            while (!m_cameraAccess.IsPlaying)
+            if (!TryResolveWebCamManager())
             {
+                yield break;
+            }
+
+            var webCamTexture = m_webCamTextureManager.WebCamTexture;
+            while (webCamTexture == null || webCamTexture.width <= 0 || webCamTexture.height <= 0)
+            {
+                webCamTexture = m_webCamTextureManager.WebCamTexture;
                 yield return null;
             }
-            m_image.texture = m_cameraAccess.GetTexture();
+
+            if (!webCamTexture.isPlaying)
+            {
+                webCamTexture.Play();
+            }
+
+            const int maxWaitFrames = 5;
+            int waitedFrames = 0;
+            do
+            {
+                yield return new WaitForEndOfFrame();
+                waitedFrames++;
+            } while (!webCamTexture.didUpdateThisFrame && waitedFrames < maxWaitFrames);
+
+            m_image.texture = webCamTexture;
+            m_streamCoroutine = null;
         }
 
         private IEnumerator Start()
         {
-            m_debugText.text = "No permission granted.";
-            while (!OVRPermissionsRequester.IsPermissionGranted(OVRPermissionsRequester.Permission.PassthroughCameraAccess))
+            if (m_debugText != null)
             {
+                m_debugText.text = "Waiting for camera...";
+            }
+
+            if (!TryResolveWebCamManager())
+            {
+                yield break;
+            }
+
+            var webCamTexture = m_webCamTextureManager.WebCamTexture;
+            while (webCamTexture == null || webCamTexture.width <= 0 || webCamTexture.height <= 0)
+            {
+                webCamTexture = m_webCamTextureManager.WebCamTexture;
                 yield return null;
             }
-            m_debugText.text = "Permission granted.";
 
-            while (!m_cameraAccess.IsPlaying)
+            if (m_debugText != null)
             {
-                yield return null;
+                m_debugText.text = "Camera ready.";
             }
             ResumeStreamingFromCamera();
+        }
+
+        private IEnumerator CaptureSnapshotCoroutine()
+        {
+            m_isCapturing = true;
+
+            if (!TryResolveWebCamManager())
+            {
+                m_isCapturing = false;
+                yield break;
+            }
+
+            var webCamTexture = m_webCamTextureManager.WebCamTexture;
+            while (webCamTexture == null || webCamTexture.width <= 0 || webCamTexture.height <= 0)
+            {
+                webCamTexture = m_webCamTextureManager.WebCamTexture;
+                yield return null;
+            }
+
+            if (!webCamTexture.isPlaying)
+            {
+                webCamTexture.Play();
+            }
+
+            const int maxWaitFrames = 5;
+            int waitedFrames = 0;
+            do
+            {
+                yield return new WaitForEndOfFrame();
+                waitedFrames++;
+            } while (!webCamTexture.didUpdateThisFrame && waitedFrames < maxWaitFrames);
+
+            var outputSize = m_webCamTextureManager.RequestedResolution;
+            if (outputSize == Vector2Int.zero)
+            {
+                outputSize = new Vector2Int(webCamTexture.width, webCamTexture.height);
+            }
+
+            if (outputSize.x <= 0 || outputSize.y <= 0)
+            {
+                Debug.LogWarning("CameraToWorldCameraCanvas: WebCamTexture not ready for snapshot.");
+                m_isCapturing = false;
+                yield break;
+            }
+
+            var rt = RenderTexture.GetTemporary(outputSize.x, outputSize.y, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+            var prev = RenderTexture.active;
+            Graphics.Blit(webCamTexture, rt);
+            RenderTexture.active = rt;
+
+            if (m_cameraSnapshot == null || m_cameraSnapshot.width != outputSize.x || m_cameraSnapshot.height != outputSize.y)
+            {
+                if (m_cameraSnapshot != null)
+                {
+                    Destroy(m_cameraSnapshot);
+                }
+                m_cameraSnapshot = new Texture2D(outputSize.x, outputSize.y, TextureFormat.RGBA32, false);
+            }
+
+            m_cameraSnapshot.ReadPixels(new Rect(0, 0, outputSize.x, outputSize.y), 0, 0, false);
+            m_cameraSnapshot.Apply(false, false);
+
+            RenderTexture.active = prev;
+            RenderTexture.ReleaseTemporary(rt);
+
+            if (m_streamCoroutine != null)
+            {
+                StopCoroutine(m_streamCoroutine);
+                m_streamCoroutine = null;
+            }
+            m_image.texture = m_cameraSnapshot;
+            m_isCapturing = false;
+        }
+
+        private bool TryResolveWebCamManager()
+        {
+            if (m_webCamTextureManager == null)
+            {
+                m_webCamTextureManager = FindAnyObjectByType<WebCamTextureManager>();
+            }
+
+            if (m_webCamTextureManager == null)
+            {
+                Debug.LogWarning("CameraToWorldCameraCanvas: WebCamTextureManager not found.");
+                if (m_debugText != null)
+                {
+                    m_debugText.text = "WebCamTextureManager not found.";
+                }
+                return false;
+            }
+
+            return true;
         }
     }
 }
